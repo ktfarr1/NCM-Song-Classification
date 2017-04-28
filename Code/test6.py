e94770111f5c2beb3cf5d301324daace206f1c10
ktfarr <ktfarr1@users.noreply.github.com>
2017-04-27 23:42:46 -0400

    Adding randomized search for hyperparameters

--- Code/test6.py
+++ Code/test6.py
@@ -1,8 +1,12 @@
 import numpy as np
 import glob
 import DataProcessing
-from sklearn import model_selection
+from time import time
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
 
+if __name__ == '__main__':
 # for b in [2,4,8,16,32,64,128,256]:
 # # for b in [1]:
 #     for p in [150820,155665,160510]:
@@ -21,6 +25,23 @@ from sklearn import model_selection
 #     np.save("../Data/All_{}.npy".format(b),full)
 #     # print np.sum(full)
 
-for b in [2,4,8,16,32,64,128,256]:
-    data = np.load("../Data/All_{}.npy".format(b))
-    Xtr, Xte, Ytr, Yte = model_selection.train_test_split(data[:,:-1],data[:,-1], test_size=0.4, train_size=0.6, random_state=0, stratify=data[:,-1])
+    for b in [2,4,8,16,32,64,128,256]:
+        start = time()
+        data = np.load("../Data/All_{}.npy".format(b))
+        print "Loaded data {} in {} sec".format(b,time()-start)
+        start = time()
+        Xtr, Xte, Ytr, Yte = train_test_split(data[:,:-1],data[:,-1], test_size=0.4, train_size=0.6, random_state=0, stratify=data[:,-1])
+        print "Split data {} in {} sec".format(b, time() - start)
+        C_s = np.logspace(-3, 3, 25)
+        G_s = np.logspace(-5,2,25,base=(data.shape[1]-1))
+        params = {"C": C_s, "gamma": G_s}
+        clf = svm.SVC(kernel='rbf', decision_function_shape='ovr')
+        # print clf.get_params().keys()
+        rand_search = RandomizedSearchCV(clf,params,n_jobs=2,refit=True,random_state=0,n_iter=10)
+        start = time()
+        rand_search.fit(Xtr, Ytr)
+        print "Random Search took {} for 20 candidates".format(time()-start)
+        print "Bin Size: {}".format(b)
+        print rand_search.best_params_
+        # print rand_search.cv_results_
+        print rand_search.best_estimator_.score(Xte,Yte)
